a2946267c7a443df0516f7c4863cf321397b1069
Anna-Mariia <vitkovska.pn@ucu.edu.ua>
2024-10-13 14:14:37 +0300

    Implementing base Flower case

---
+++ src/main/java/laboratories/Flower.java
@@ -0,0 +1,31 @@
+package laboratories;
+
+public class Flower {
+    private double sepalLength;
+    private String color;
+    private double price;
+
+    public void setSepalLength(double sepalLength) {
+        this.sepalLength = sepalLength;
+    }
+
+    public double getSepalLength() {
+        return sepalLength;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
